1a8417f08a8c3a0a5acbdfa521ce25437a65a29c
nuuuwan <nuuuwan@gmail.com>
2023-05-19 10:58:57 +0530

    class ChatWrapper

---
+++ src/gpttools/core/ChatWrapper.py
@@ -0,0 +1,34 @@
+import os
+
+import openai
+
+from gpttools.core.Message import Message
+
+openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Say this is a test",
+    max_tokens=7,
+    temperature=0,
+)
+
+
+class ChatWrapper:
+    def __init__(self):
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.messages = []
+
+    def send(self, content):
+        self.messages.append(Message(role="user", content=content).todict())
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo', messages=self.messages
+        )
+        return response['choices'][0]['message']['content']
+
+
+if __name__ == "__main__":
+    chat = ChatWrapper()
+    while True:
+        content = input("> ")
+        if content == 'x':
+            break
+        print(chat.send(content))
